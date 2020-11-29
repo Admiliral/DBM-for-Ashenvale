a80b555a835b10e62394c1e154bcd557ade29ea9
Admiiral <Admiiral@.github.com>
2020-11-29 19:54:41 +0500

    2

---
+++ Основы C#/Homework 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    class Homework_1
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
